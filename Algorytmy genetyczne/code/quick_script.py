52f44910a36ec65616bd6aa31d99af92e8dc0213
Witold Frącek <witek.fracek@gmail.com>
2022-03-05 13:03:32 +0100

    static data loading

---
+++ Algorytmy genetyczne/code/quick_script.py
@@ -0,0 +1,9 @@
+import numpy as np
+from staticData import StaticData, COST_PATH, FLOW_PATH
+
+if __name__ == '__main__':
+    sd = StaticData()
+    sd.prepare_matrices()
+
+
+
---
+++ Algorytmy genetyczne/code/staticData.py
@@ -0,0 +1,62 @@
+import numpy as np
+import json
+
+EASY = 'easy'
+FLAT = 'flat'
+HARD = 'hard'
+
+# Set exercise mode
+MODE = EASY
+
+COST_PATH = f'../data/{MODE}_cost.json'
+FLOW_PATH = f'../data/{MODE}_flow.json'
+
+
+class StaticData:
+    def __init__(self):
+        self.cost_matrix = np.zeros((1, 1))
+        self.flow_matrix = np.zeros((1, 1))
+        self.value_matrix = np.zeros((1, 1))
+
+    def __get_list(self, path):
+        with open(path, encoding='utf-8') as file:
+            loaded_dict = json.load(file)
+        return loaded_dict
+
+    def __check_machine_count(self, data_list):
+        machine_list = set()
+        for d in data_list:
+            machine_list.add(d['source'])
+            machine_list.add(d['dest'])
+        return max(machine_list)
+
+    def prepare_matrices(self):
+        costs = self.__get_list(COST_PATH)
+        flows = self.__get_list(FLOW_PATH)
+        count = self.__check_machine_count(costs) + 1
+        self.cost_matrix = np.zeros((count, count), dtype=np.int64)
+        self.flow_matrix = np.zeros((count, count), dtype=np.int64)
+        self.__assign_cost(costs)
+        self.__assign_flow(flows)
+        self.value_matrix = self.cost_matrix * self.flow_matrix
+
+    def __assign_cost(self, data_list):
+        for d in data_list:
+            source = d['source']
+            dest = d['dest']
+            cost = d['cost']
+            self.cost_matrix[source, dest] = cost
+
+    def __assign_flow(self, data_list):
+        for d in data_list:
+            source = d['source']
+            dest = d['dest']
+            flow = d['amount']
+            self.flow_matrix[source, dest] = flow
+
+    # temp public
+    def get_list(self, path):
+        return self.__get_list(path)
+
+    def cmc(self, path):
+        return self.__check_machine_count(path)
