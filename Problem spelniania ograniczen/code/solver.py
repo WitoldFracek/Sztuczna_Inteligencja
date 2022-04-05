ca66e4f10d068347e196710e815126e93daf9d6f
Witold Frącek <witek.fracek@gmail.com>
2022-04-05 16:36:15 +0200

    forward lookup code cleanup

--- Problem spelniania ograniczen/code/solver.py
+++ Problem spelniania ograniczen/code/solver.py
@@ -36,23 +36,9 @@ class GridCSPSolver(CSPSolver):
                 value = variable.available_values.pop()
                 if all([constraint(self.__grid, variable, value) for constraint in self.__constraints]):
                     variable.value = value
-                    # pretty_binary_print(self.__grid)
-                    # for variable in self.__variables[depth_index + 1:]:
-                    #     print(variable.position, variable.available_values)
-                    # print()
-                    # if forward_check:
-                    #     if self.__forward_check(depth_index):
-                    #         depth_index += 1
-                    #     else:
-                    #         variable.value = None
-                    # else:
-                    #     depth_index += 1
                     depth_index += 1
                     if depth_index == len(self.__variables):
                         solutions.append(copy.deepcopy(self.__grid))
-                        # xs = Qlist(*self.__constraints).where(lambda cons: hasattr(cons, "inequalities"))[0].inequalities
-                        # pretty_futoshiki_print(self.__grid, xs)
-                        # print()
                         if variable.all_checked and forward_is_ok:
                             forward_is_ok = True
                             self.__rollback(variable)
@@ -96,19 +82,6 @@ class GridCSPSolver(CSPSolver):
         eliminator_id = self.__variables[eliminator_index].id
         for variable in self.__variables[eliminator_index + 1:]:
             variable.recover_from_history(eliminator_id)
-
-    def __forward_check(self, eliminator_index: int):
-        eliminator_id = self.__variables[eliminator_index].id
-        for variable in self.__variables[eliminator_index + 1:]:
-            variable.eliminate(self.__grid, self.__constraints, eliminator_id)
-        if any([variable.all_checked for variable in self.__variables[eliminator_index + 1:]]):
-            for variable in self.__variables[eliminator_index + 1:]:
-                variable.recover_from_history(eliminator_id)
-            return False
-        return True
-
-    def index_to_coordinates(self, index: int):
-        return index // self.__size, index % self.__size
 
 
 
